e23a7b07639094d034dc756bca642d375e02335f
amimenezes <mi.menezes.luiz@gmail.com>
2022-04-03 21:44:44 -0300

    Adicina projeto base no Github

---
+++ src/main/java/edu/ifrs/pw2/webservice/calculator/Pw2webservicecalculatorRestApplication.java
@@ -0,0 +1,11 @@
+package edu.ifrs.pw2.webservice.calculator;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class Pw2webservicecalculatorRestApplication extends Application {
+}
